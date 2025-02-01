517f4a444350cf0b838c89be8edd520b712f2681
Victor Efrain Alarcon Olmedo <alarconolmedo99@gmail.com>
2025-01-31 22:42:17 -0500

    correccion

--- pruebas.js
+++ pruebas.js
@@ -1,10 +1,13 @@
 let butSend = document.getElementsByClassName('button-add')[0]
 let inp = document.getElementsByClassName('input-name')[0];
-let listAmigo = document.getElementsByClassName('result-list')[0];
-let nombre = document.getElementsByClassName('nombre-lista');
+let listAmigo = document.getElementsByClassName('name-list')[0];
+let resultList = document.getElementsByClassName('result-list')[0];
+let nombrelista = document.getElementsByClassName('nombre-lista');
+let nombreEleccion = document.getElementsByClassName('nombre-eleccion');
 let buttonDraw = document.getElementsByClassName('button-draw')[0];
 
 
+let list1 = []
 
 
 
@@ -12,19 +15,35 @@ let buttonDraw = document.getElementsByClassName('button-draw')[0];
 
 butSend.addEventListener('click', (e)=>{
  e.preventDefault()
- let amigo = document.createElement('li'); // Crear un elemento <li>
+ while (nombreEleccion.length > 0) {
+  nombreEleccion[0].parentNode.removeChild(nombreEleccion[0]); // Elimina el primer elemento hasta que no quede ninguno
+ }
+ if (inp.value != '') {
+  let amigo = document.createElement('li'); // Crear un elemento <li>
  amigo.className = 'nombre-lista'
  amigo.textContent = inp.value; // Asignar el texto ingresado en el input
+ list1.push(inp.value)
  listAmigo.appendChild(amigo); // Agregar el <li> a la lista
-  
+ inp.value = ''
+ } else {
+  alert('debes ingresar un nombre correcto')
+ }
 })
 
 
 buttonDraw.addEventListener('click', (e)=>{
  e.preventDefault();
- if (nombre.length > 0) {
-  if ((nombre.length % 2) == 0 ) {
-   alert('se puede')
+ if (nombrelista.length > 0) {
+  if ((nombrelista.length % 2) == 0 ) {
+   while (nombrelista.length > 0) {
+    nombrelista[0].parentNode.removeChild(nombrelista[0]); // Elimina el primer elemento hasta que no quede ninguno
+   }
+   let nomSort = Math.floor(Math.random()*list1.length)
+   let amigo = document.createElement('li');
+   amigo.className = 'nombre-eleccion';
+   amigo.textContent = `tu amigo secreto es ${list1[nomSort]}`;
+   resultList.appendChild(amigo)
+   list1 = []
   } else (
    alert('para que se pueda hacer el sorteo la cantidad de participantes debe ser par')
   )
@@ -32,3 +51,7 @@ buttonDraw.addEventListener('click', (e)=>{
   alert('ingresa los nombres para hacer el emparejamiento')
  }
 })
+
+
+
+
